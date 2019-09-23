f6de4aceb9590d030f24e6266cc93a74e2d3f7aa
juntaolei <44855944+juntaolei@users.noreply.github.com>
2019-09-23 17:56:52 -0400

    removed and changed some unnecessary code

--- 10_occ/randoccupation.py
+++ 10_occ/randoccupation.py
@@ -1,16 +1,23 @@
+# Jun Tao Lei (Of TeamKubica With Calvin Chu)
+# SoftDev1 pd9
+# K#10 -- Template Displaying Random Occupations / Dictionary + Flask + Jinja2 / Display a table of occupations and a random occupation.
+# 2019-09-20
+
 from csv import DictReader as dr
 from random import choices as rc
-from collections import OrderedDict
 
+# Read the csv file and added occupations to a dictionary row by row.
 def getoccupations(file):
   dic = {}
   for row in dr(open(file)):
-    dic[row["Job Class"]] = float(row["Percentage"])
+    dic[row["Job Class"]] = [float(row["Percentage"])]
   return dic
 
+# Gets the percentages from the dictionary and use choices() to pick an occupation.
 def randoccupation(dic):
-  tmp = OrderedDict(dic.copy())
-  tmp.pop("Total")
+  tmp = dic.copy()
+  tmp.pop("Total") # Remove Total and 99.8 from the temporary dictionary.
   keys = list(tmp.keys())
   values = [value[0] for value in tmp.values()]
-  return rc(keys, values)[0]
+  # Associates occupations to corresponding percentages.
+  return rc(keys, values)[0] # Return an occupation based on the percentages.
--- 10_occ/urlify.py
+++ 10_occ/urlify.py
@@ -1,5 +1,10 @@
+# Jun Tao Lei (Of TeamKubica With Calvin Chu)
+# SoftDev1 pd9
+# K#10 -- Template Displaying Random Occupations / Dictionary + Flask + Jinja2 / Display a table of occupations and a random occupation.
+# 2019-09-20
+
+# Append an url to the values associated with each key.
 def urlify(dic, url):
-  tmp = dic.copy()
-  for key in tmp:
-    tmp[key] = [tmp[key], url + key]
-  return tmp
+  for key in dic:
+    key = dic[key].append(url + key)
+  return dic
