edeaa4d8132b9037683c1dffe2cea2cfed49e75e
juntaolei <44855944+juntaolei@users.noreply.github.com>
2019-09-26 14:44:02 -0400

    finishing it up

--- 12_form/app.py
+++ 12_form/app.py
@@ -1,26 +1,26 @@
-#Nahi Khan, Connor Oh, Winston Peng [Team Beaker]
-
-#SoftDev1 pd9
-
-#K10 -- Jinja Tuning
-
-#2019 - 09 - 23
+# Jun Tao Lei & Connor Oh
+# SoftDev1 pd9
+# K12 -- Jinja Forming
+# 2019-09-25
 
 from flask import Flask, render_template, request
 
-app = Flask(__name__)                                       #instantiate Flask
+# Instantiate Flask
+app = Flask(__name__)                                       
 
-@app.route("/")                                #the route we will be using to access our table
+@app.route("/")                                
 def foo():
-    print(app)
-    return render_template(
-        'foo.html'
-    )
+  return render_template("foo.html")
 
 @app.route("/auth")
 def auth():
-    print(request)
-    print(request.args)
-    return "ok"
+  print(app)
+  print(request)
+  print(request.args)
+  print(request.args["username"])
+  print("HEADERSSSSS")
+  print(request.headers)
+  name = request.args["username"]
+  return render_template("auth.html", user = name)
 
 app.run(debug = True)
