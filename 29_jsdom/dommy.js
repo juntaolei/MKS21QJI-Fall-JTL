7e441eeeaf9cd62ef231c474e4508ebc1030cb72
juntaolei <44855944+juntaolei@users.noreply.github.com>
2019-12-15 13:51:33 -0500

    a

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -1,63 +1,57 @@
-let thefiblist = [0, 1], current = 0;
+let fibsequence = [0, 1], current = 0;
 
 const lis = document.getElementsByTagName("li");
-
 const lis2 = document.getElementById("fiblist");
 
-const changeHeading = (e) => {
-  document.getElementById("h").innerHTML = e;
-};
+const changeHeading = e => { document.getElementById("h").innerHTML = e; };
 
-const removeItem = (e) => {
-  e.remove();
-};
+const removeItem = e => { e.remove(); };
 
 Array.from(lis).forEach(
-  (i) => {
+  i => {
     i.addEventListener("mouseover", () => changeHeading(i.innerHTML));
     i.addEventListener("mouseout", () => changeHeading("Hello World!"));
     i.addEventListener("click", () => removeItem(i));
   }
 );
 
-const addItem = (e) => {
-  let list = document.getElementById("thelist");
+const addItem = () => {
   let item = document.createElement("li");
   item.innerHTML = "WORD";
   item.addEventListener("mouseover", () => changeHeading(item.innerHTML));
   item.addEventListener("mouseout", () => changeHeading("Hello World!"));
   item.addEventListener("click", () => removeItem(item));
-  list.appendChild(item);
+  document.getElementById("thelist").appendChild(item);
 };
 
-document.getElementById("b").addEventListener("click", () => addItem());
-
-const fib = (n) => {
-  if (n < 2)
-    return 1;
+const fib = n => {
+  if (n < 2) return 1;
   return fib(n - 1) + fib(n - 2);
 }
 
-const fibonacci = (n, m = thefiblist) => {
+const fibonacci = (n, m = fibsequence) => {
   if (n >= m.length)
     m[n] = fibonacci(n - 1) + fibonacci(n - 2);
   return m[n];
 };
 
-const addFib = (e) => {
-  // console.log(e);
-  let thethefiblist = document.getElementById("fiblist");
+const addFib = current => {
   let item = document.createElement("li");
-  item.innerHTML = fib(current++);
-  thethefiblist.appendChild(item);
+  item.addEventListener("mouseover", () => changeHeading(item.innerHTML));
+  item.addEventListener("mouseout", () => changeHeading("Hello World!"));
+  item.addEventListener("click", () => removeItem(item));
+  item.innerHTML = fib(current);
+  document.getElementById("fiblist").appendChild(item);
 };
 
-const addFib2 = (e) => {
-  // console.log(e);
-  let thethefiblist = document.getElementById("fiblist");
+const addFib2 = current => {
   let item = document.createElement("li");
-  item.innerHTML = fibonacci(current++);
-  thethefiblist.appendChild(item);
+  item.addEventListener("mouseover", () => changeHeading(item.innerHTML));
+  item.addEventListener("mouseout", () => changeHeading("Hello World!"));
+  item.addEventListener("click", () => removeItem(item));
+  item.innerHTML = fibonacci(current);
+  document.getElementById("fiblist").appendChild(item);
 };
 
-document.getElementById("fb").addEventListener("click", () => addFib2());
+document.getElementById("b").addEventListener("click", () => addItem());
+document.getElementById("fb").addEventListener("click", () => addFib2(current++));
