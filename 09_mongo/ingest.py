9590215fadf5586e2c10998e2ae7eed897d85137
Jun Tao Lei <44855944+juntaolei@users.noreply.github.com>
2020-02-27 19:24:42 -0500

    Update ingest.py

--- 09_mongo/ingest.py
+++ 09_mongo/ingest.py
@@ -2,4 +2,4 @@ from bson.json_util import loads
 
 def ingest(f):
     with open(f) as _f:
-        return loads(f'[{",".join(map(lambda s: s[:len(s) - 1], _f))}]')
+        return loads(f'[{",".join(map(lambda s: s[:-1], _f))}]')
