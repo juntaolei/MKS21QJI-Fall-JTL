dd60d5c54c30b13c8d072f488916cbe4e61ef648
juntaolei <44855944+juntaolei@users.noreply.github.com>
2020-03-02 22:13:52 -0500

    Update

--- 10_mongo/licenses.py
+++ 10_mongo/licenses.py
@@ -1,24 +1,22 @@
 # functions to query data
 find_by_name = lambda database, name: list(
-  database['license'].find({
-    'name': name
-  })
+  database['license'].find({'name': name})
 )
 find_by_identifier = lambda database, identifier: list(
   database['license'].find({
-    'identifiers': {
-      '$elemMatch': {
-        "identifier": identifier
-      }
-    }
+    'identifiers': { '$elemMatch': { "identifier": identifier } }
   })
 )
-find_by_keyword = lambda database, keyword: list(
+find_by_keywords = lambda database, keywords: list(
+  chain.from_iterable(filter(None, list(map(
+          lambda keyword: list(
+            database['license'].find({
+              'keywords': { '$elemMatch': { '$eq': keyword } }
+            })
+          ), keywords
+        )))))
+find_by_filter = lambda database, _filter: list(
   database['license'].find({
-    'keywords': {
-      '$elemMatch': {
-        '$eq': keyword
-      }
-    }
+    'keywords:': { '$nin': [_filter] }
   })
 )
